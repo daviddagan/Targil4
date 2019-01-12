737b56c1c6601967fafe01c7f36b86f910f320e9
adir abargil <adir1661@users.noreply.github.com>
2019-01-12 23:39:46 +0200

    12.1.19 work together on teamvieewer

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,98 @@
-$(".tab-item-t4").on('click',function(){
-    let $this=$(this);
+$(".tab-item-t4").on('click', function () {
+    let $this = $(this);
+    if ($this.hasClass("disabled1")) return;
     $(".tab-item-t4").removeClass("active");
     $this.addClass("active");
+    let index = $(".tab-item-t4").index($this);
+    switch (index) {
+        case 0:
+            catalogView();
+            break;
+        case 1:
+            branchView();
+            break;
+        case 2:
+            usersView();
+            break;
+        case 3:
+            aboutView();
+            break;
+        case 4:
+            contactView();
+            break;
+        default:
+            break;
+
+    }
+
 });
+
+function catalogView() {
+
+
+}
+
+function branchView() {
+
+}
+
+function usersView() {
+
+}
+
+function aboutView() {
+    console.log("activated")
+    $.ajax({
+        url: "about", success: function (result) {
+            $(".main-bg").html(result);
+        }
+    });
+}
+
+function contactView() {
+
+}
+
+aboutView();
+
+function login(showhide) {
+    if (showhide == "show") {
+        document.getElementById('popupbox').style.visibility = "visible";
+    } else if (showhide == "hide") {
+        document.getElementById('popupbox').style.visibility = "hidden";
+    }
+}
+
+$(function () {
+    $("#loginForm").submit(function (event) {
+        let password = $("#passwordF").val();
+        let username = $("#usernameF").val();
+        let sendInfo = {username, password};
+        $.ajax({
+            url: "login",
+            type: "POST",
+            dataType: "json",
+            success: function (result) {
+                if(result.message ==="manager"){
+                    console.log(result,"logged");
+                    let tabs =$(".tab-item-t4");
+                    tabs.get(0).removeClass("disabled1");
+                    tabs.get(1).removeClass("disabled1");
+                    tabs.get(2).removeClass("disabled1");
+                }
+            },
+            data: sendInfo
+        });
+        login("hide");
+        return false;
+    });
+});
+
+function logout() {
+    $.ajax({url:"/reset"})
+}
+// catalog
+// branch managment
+// user managment
+// about
+// contact
--- server.js
+++ server.js
@@ -1,21 +1,56 @@
 let path = require('path');
 let express = require('express');
 let app = express();
+let bodyParser = require("body-parser");
 let db = require("./database.json");
+let loginFlag = false,
+    managerFlag = false;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     let line = "Now We will present to you our students (): ";
-    res.render('index', {tagline: line });
+    res.render('index', {tagline: line, loginFlag, managerFlag});
 });
-
-app.get('/about', function(req, res) {
-  res.render('about');
+app.post("/login", function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    let empty = [];
+    let managers = db.user.manager;
+    let flag = false;
+    managers.forEach(function (manager) {
+        if (manager.name === username && manager.password === password) {
+            res.send(JSON.stringify({"message":"manager"}));
+            console.log("manager");
+            managerFlag = true;
+            loginFlag = true;
+            flag = true;
+        }
+    });
+    let users = empty.concat(db.user.worker).concat(db.user.client);
+    users.forEach(function (item) {
+        if (item.name === username && item.password === password) {
+            res.send(JSON.stringify({"message":"user"}));
+            console.log("user");
+            loginFlag = true;
+            flag = true;
+        }
+    })
+    if (!flag) {
+        console.log({"message":"not user"});
+        res.send(JSON.stringify({"message":"not user"}));
+    }
 });
-
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+app.get('/reset', function (req,res){
+    loginFlag = false;
+    managerFlag = false;
+    res.send("done!")
+})
 app.listen(8080, function () {
-  console.log('Listening on port 8080!');
+    console.log('Listening on port 8080!');
 });
