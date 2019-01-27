d4b5692227c17688407fb775eb0691b433c63001
adir abargil <adir1661@users.noreply.github.com>
2019-01-27 21:41:22 +0200

    ajax for the form sign....

--- public/js/users.js
+++ public/js/users.js
@@ -1,32 +1,33 @@
+let userClickedName;
+
 function usersView() {
     queryAndUpdate("users");
 }
 
-$(".main-bg").on('click',".editUserButton", function () {
+$(".main-bg").on('click', ".editUserButton", function () {
     let $this = $(this);
     let index = $(".editUserButton").index($this);
-    var username =$this.parent().parent().find(".editUserName");
+    var username = $this.parent().parent().find(".editUserName");
     $(".list-group-item").css({"border-color": "rgba(0,0,0,.125)"});
     $this.parent().parent().css({"border-color": "green"});
     $.ajax({
-        url:"users/get/"+username.text(),
-        success: function(user){
-            if(user.role==="manager")
+        url: "users/get/" + username.text(),
+        success: function (user) {
+            if (user.role === "manager")
                 managerUpdate(user);
-            else if(user.role==="worker")
+            else if (user.role === "worker")
                 workerUpdate(user);
-            else 
+            else
                 clientUpdate(user);
+            userClickedName = user.name;
             optionChanged()
         }
     })
-    
 
-    
 
 })
 
-function managerUpdate(user){
+function managerUpdate(user) {
     $("#selectUser").val(user.role);
     $("#one").val(user.name);
     $("#two").val(user.password);
@@ -40,7 +41,7 @@ function managerUpdate(user){
     $("#L4").text("גיל");
 }
 
-function workerUpdate(user){
+function workerUpdate(user) {
     $("#selectUser").val(user.role);
     $("#one").val(user.name);
     $("#two").val(user.password);
@@ -51,15 +52,15 @@ function workerUpdate(user){
     $("#L2").text("סיסמא");
     $("#L3").text("כינוי");
     $("#L4").text("משכורת");
-   
+
 }
 
-function clientUpdate(user){
+function clientUpdate(user) {
     $("#selectUser").val(user.role);
     $("#one").val(user.name);
     $("#two").val(user.favorite);
     $("#three").val(user.password);
-    $("#four").val(user.phone);  
+    $("#four").val(user.phone);
 
     $("#L2").text("סניף מועדף");
     $("#L3").text("סיסמא");
@@ -67,56 +68,64 @@ function clientUpdate(user){
 }
 
 
+function optionChanged() {
+    var photographyTypeElement = document.getElementById("selectUser");
+    var selectedValue = photographyTypeElement.options[photographyTypeElement.selectedIndex].value;
+    var optionalValues = ["manager", "worker", "client"];
 
-
-
-
-
-    function optionChanged() {
-      var photographyTypeElement = document.getElementById("selectUser");
-      var selectedValue = photographyTypeElement.options[photographyTypeElement.selectedIndex].value;
-      var optionalValues = ["manager", "worker","client"];
-      
-        if (selectedValue=="manager")
-        {
-          document.getElementById("rankId").style.display = "" ;     
-        }   
-        else
-        {
-          document.getElementById("rankId").style.display = "none" ;
-        }                
-      }
-
-
-
-      function addUser(){
-        $(".main-bg").on('submit',"#formUser", function () {
-            let user ;
-
-            if(user.role==="manager"){
-                 user.role =  $("#selectUser").val();
-                 user.name = $("#one").val();
-                 user.password = $("#two").val();
-                 user.branch = $("#three").val();
-                 user.age = $("#four").val(); 
-                 user.rank = $("#five").val(); 
-                 } 
-            else if(user.role==="worker"){
-                 user.role =  $("#selectUser").val();
-                 user.name = $("#one").val();
-                 user.favorite =  $("#two").val();
-                 user.password = $("#three").val();
-                 user.phone = $("#four").val(); 
-                 } 
-            else {
-                 user.role =  $("#selectUser").val();
-                 user.name = $("#one").val();
-                 user.favorite =  $("#two").val();
-                 user.password = $("#three").val();
-                 user.phone = $("#four").val(); 
-                 } 
-
-        })
+    if (selectedValue == "manager") {
+        document.getElementById("rankId").style.display = "";
     }
-    
+    else {
+        document.getElementById("rankId").style.display = "none";
+    }
+}
+
+
+$(".main-bg").on('submit', "#formUser", function () {
+    let user = {};
+    let role = $("#selectUser").val();
+    if (role === "manager") {
+        user.role = $("#selectUser").val();
+        user.name = $("#one").val();
+        user.password = $("#two").val();
+        user.branch = $("#three").val();
+        user.age = $("#four").val();
+        user.rank = $("#five").val();
+    }
+    else if (role === "worker") {
+        user.role = $("#selectUser").val();
+        user.name = $("#one").val();
+        user.favorite = $("#two").val();
+        user.password = $("#three").val();
+        user.phone = $("#four").val();
+    }
+    else if (role === "client") {
+        user.role = $("#selectUser").val();
+        user.name = $("#one").val();
+        user.favorite = $("#two").val();
+        user.password = $("#three").val();
+        user.phone = $("#four").val();
+    }
+    let container = $(".edit-form-conatiner");
+    progressAnimation.start(container)
+    $.ajax({
+        url: "userUpd/" + userClickedName,
+        body: user,
+        success: function (result) {
+            progressAnimation.stop(container, function () {
+                $("#formUser")[0].reset();
+                userClickedName = '';
+            })
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+            progressAnimation.stop(container, function () {
+                $(".main-bg").prepend("<p>there was an error on the server....</p>")
+            })
+        }
+    });
+    return false;
+})
+
  
